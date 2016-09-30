0b09e3abb326ce0b99a60da496e47d04a91679a4
Matt Windsor <mbw500@york.ac.uk>
2016-09-30 19:57:32 +0100

    Start making things.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+'use strict'
+
+var connectBtn = document.querySelector('#custom-studio-connect');
+var connectStatus = document.querySelector('#custom-studio-status');
+
+connectBtn.addEventListener('click',
+    function() {
+        connectStatus.textContent = "woot!";
+    }
+);
