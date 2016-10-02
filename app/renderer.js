c629ab90144c0369df0c752ed0d9284123e9b85f
Matt Windsor <mattwindsor@btinternet.com>
2016-10-02 14:38:18 +0100

    Give up for now.

--- app/renderer.js
+++ app/renderer.js
@@ -25,3 +25,11 @@ connectBtn.addEventListener('click',
     );
   }
 );
+
+(function(app) {
+  document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic
+      .platformBrowserDynamic()
+      .bootstrapModule(app.AppModule);
+  });
+})(window.app || (window.app = {}))
